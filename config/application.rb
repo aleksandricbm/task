0c539e308cfc730160779094f269d52447d624af
Aleksander <aleksander.ignatkin@gmail.com>
2016-12-05 15:51:42 +0200

    Heroku assets

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module TaskManager
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.serve_static_assets = true
   end
 end
